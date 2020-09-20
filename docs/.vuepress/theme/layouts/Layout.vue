0d50d7e7a7b858fe5024510bf5b821ee6727037b
eddievandeer <1255173159@qq.com>
2020-09-20 20:14:12 +0800

    fix:滚动时存在的问题

--- docs/.vuepress/theme/layouts/Layout.vue
+++ docs/.vuepress/theme/layouts/Layout.vue
@@ -9,7 +9,20 @@
 <script>
       import blogHome from '@theme/components/blogHome'
       export default {
+            data() {
+                  return {
+                        interval: null
+                  }
+            },
             methods: {
+                  stopScroll(e) {
+                        let event = e || window.event
+                        if (event.preventDefault) {
+                              event.preventDefault()
+                        } else {
+                              event.returnValue = false
+                        }
+                  },
                   onScroll(e) {
                         if (window.location.pathname != '/') {
                               return
@@ -19,13 +32,24 @@
                         let home = document.querySelector('.blog-home')
                         let about = document.querySelector('#about')
                         let scrolled = document.documentElement.scrollTop || document.body.scrollTop
+
+                        let main = document.querySelector('.theme-container')
+
                         if (event.wheelDelta < 0 || event.detail > 0) {
                               if (scrolled <= about.offsetTop - 1) {
                                     if (event.preventDefault) {
                                           event.preventDefault()
+                                          event.stopPropagation()
                                     } else {
+                                          evt.cancelBubble = true
                                           event.returnValue = false
                                     }
+                                    main.removeEventListener('mousewheel', this.onScroll, false)
+                                    main.removeEventListener('DOMMouseScroll', this.onScroll, false)
+                                    main.removeEventListener('wheel', this.onScroll, false)
+                                    main.addEventListener('mousewheel', this.stopScroll, false)
+                                    main.addEventListener('DOMMouseScroll', this.stopScroll, false)
+                                    main.addEventListener('wheel', this.stopScroll, false)
                                     window.scrollTo(0, about.offsetTop)
                                     header.classList.remove('hide')
                               }
@@ -33,13 +57,31 @@
                               if (scrolled <= about.offsetTop + 60) {
                                     if (event.preventDefault) {
                                           event.preventDefault()
+                                          event.stopPropagation()
                                     } else {
+                                          evt.cancelBubble = true
                                           event.returnValue = false
                                     }
+                                    main.removeEventListener('mousewheel', this.onScroll, false)
+                                    main.removeEventListener('DOMMouseScroll', this.onScroll, false)
+                                    main.removeEventListener('wheel', this.onScroll, false)
+                                    main.addEventListener('mousewheel', this.stopScroll, false)
+                                    main.addEventListener('DOMMouseScroll', this.stopScroll, false)
+                                    main.addEventListener('wheel', this.stopScroll, false)
                                     window.scrollTo(0, 0)
                                     header.classList.add('hide')
                               }
                         }
+                        this.interval = setTimeout((scrolled) => {
+                              let main2 = document.querySelector('.theme-container')
+                              main2.removeEventListener('mousewheel', this.stopScroll, false)
+                              main2.removeEventListener('DOMMouseScroll', this.stopScroll,
+                                    false)
+                              main2.removeEventListener('wheel', this.stopScroll, false)
+                              main2.addEventListener('mousewheel', this.onScroll, false)
+                              main2.addEventListener('DOMMouseScroll', this.onScroll, false)
+                              main2.addEventListener('wheel', this.onScroll, false)
+                        }, 500)
                         return false
                   },
                   mobileScroll() {
@@ -67,7 +109,7 @@
                   })
 
                   main.addEventListener('mousewheel', this.onScroll, false)
-                  main.addEventListener('DOMMouseScroll', this.onScroll)
+                  main.addEventListener('DOMMouseScroll', this.onScroll, false)
                   main.addEventListener('wheel', this.onScroll, false)
                   main.addEventListener('touchmove', this.mobileScroll, false)
             },
