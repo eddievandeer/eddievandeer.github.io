b14e7b6e0fecd6676284bab8aba040d2f358d8d8
eddievandeer <1255173159@qq.com>
2021-06-03 17:07:50 +0800

    feat: remove blog-home to layout Show blog-articles in home page Add mave animation to blog-home

--- docs/.vuepress/theme/components/blogHome.vue
+++ docs/.vuepress/theme/components/blogHome.vue
@@ -1,210 +1,212 @@
 <template>
-      <div class="home-page" ref="main">
-            <div class="blog-home">
-                  <div class="home-content">
-                        <!-- <div class="blog-icon"></div> -->
-                        <div class="blog-description">
-                              <h1>{{ $page.frontmatter.title }}</h1>
-                              <p>本博客网站基于HTML5开发，若无法正常访问请更换高版本浏览器</p>
-                        </div>
-                  </div>
-                  <drop-down></drop-down>
-            </div>
-            <about-me></about-me>
-            <blog-footer></blog-footer>
-      </div>
+    <div class="home-page">
+        <blog-header></blog-header>
+        <div class="blog-articles">
+            <blog-articles :pageNumber="pageNumber" :filted="pages" path="page"></blog-articles>
+        </div>
+        <blog-footer></blog-footer>
+    </div>
 </template>
 
 <script>
-      import dropDown from './dropDown'
-      import aboutMe from './aboutMe'
+    import blogArticles from './blogArticles'
 
-      export default {
-            name: 'blogHome',
-            components: {
-                  dropDown,
-                  aboutMe
+    export default {
+        name: 'blogHome',
+        components: {
+            blogArticles
+        },
+        props: ['pageNumber'],
+        data() {
+            return {
+                main: null,
+                pages: [],
+                scrollOffset: 0,
+                mouseEvent: '',
+                inSlides: true,
+                slidesLock: false
+            }
+        },
+        mounted() {
+            this.bindEvent()
+        },
+        beforeMount() {
+            this.pages = this.$site.pages.filter(v => {
+                return v.path.endsWith('html')
+            })
+        },
+        methods: {
+            bindEvent() {
+                this.main = document.querySelector('.theme-container')
+                this.mouseEvent =
+                    document.onmousewheel !== undefined ?
+                    "mousewheel" :
+                    "DOMMouseScroll"
+
+                if (this.mouseEvent == "mousewheel") {
+                    this.main.addEventListener(
+                        "mousewheel",
+                        e => {
+                            let y = -0.83 * e.wheelDelta
+                            this.onMouseWheel(e, y)
+                        },
+                        false
+                    );
+                } else if (this.mouseEvent == "DOMMouseScroll") {
+                    this.main.addEventListener(
+                        "DOMMouseScroll",
+                        e => {
+                            console.log(e)
+                            this.onMouseWheel(e, e.detail)
+                        },
+                        false
+                    );
+                    this.main.addEventListener(
+                        "MozMousePixelScroll",
+                        e => {
+                            console.log(e)
+                            this.onMouseWheel(e, e.detail)
+                        },
+                        false
+                    );
+                } else {
+                    this.main.addEventListener(
+                        this.mouseEvent,
+                        e => {
+                            let y = e.deltaY
+                            if (e.deltaMode == 1) {
+                                y = e.deltaY * 32
+                            }
+                            this.onMouseWheel(e, y)
+                        },
+                        false
+                    );
+                }
             },
-            data() {
-                  return {
-                        scrollOffset: 0,
-                        mouseEvent: '',
-                        inSlides: true,
-                        slidesLock: false
-                  }
+            onMouseWheel(e, deltaY) {
+                let event = e || window.event
+                let offset = this.main.getBoundingClientRect().top
+
+                if (!this.ifInSlide(deltaY, offset)) return
+
+                this.scrollOffset = document.documentElement.scrollTop || document.body.scrollTop
+
+                if (event.preventDefault) {
+                    event.preventDefault()
+                    event.stopPropagation()
+                } else if (document.all) {
+                    event.cancelBubble = true
+                    event.returnValue = false
+                } else {
+                    event.stopPropagation()
+                }
+
+                if ((window.location.pathname != '/' || this.slidesLock) && window.location.pathname.split('/')[1] != 'page') return
+
+                this.slidesLock = true
+
+                this.doScroll(deltaY)
             },
-            mounted() {
-                  this.bindEvent()
+            doScroll(deltaY) {
+                let header = document.querySelector('.blog-header')
+                let home = document.querySelector('.blog-home')
+                if (deltaY > 0) {
+                    window.scrollTo(0, home.scrollHeight)
+                    header.classList.remove('hide')
+                } else {
+                    window.scrollTo(0, 0)
+                    header.classList.add('hide')
+                }
+
+                this.interval = setTimeout(() => {
+                    this.slidesLock = false
+                }, 500)
             },
-            methods: {
-                  bindEvent() {
-                        let main = this.$refs.main
-                        this.mouseEvent =
-                              document.onmousewheel !== undefined ?
-                              "mousewheel" :
-                              "DOMMouseScroll"
-
-                        if (this.mouseEvent == "mousewheel") {
-                              main.addEventListener(
-                                    "mousewheel",
-                                    e => {
-                                          let y = -0.83 * e.wheelDelta
-                                          this.onMouseWheel(e, y)
-                                    },
-                                    false
-                              );
-                        } else if (this.mouseEvent == "DOMMouseScroll") {
-                              main.addEventListener(
-                                    "DOMMouseScroll",
-                                    e => {
-                                          console.log(e)
-                                          this.onMouseWheel(e, e.detail)
-                                    },
-                                    false
-                              );
-                              main.addEventListener(
-                                    "MozMousePixelScroll",
-                                    e => {
-                                          console.log(e)
-                                          this.onMouseWheel(e, e.detail)
-                                    },
-                                    false
-                              );
-                        } else {
-                              main.addEventListener(
-                                    this.mouseEvent,
-                                    e => {
-                                          let y = e.deltaY
-                                          if (e.deltaMode == 1) {
-                                                y = e.deltaY * 32
-                                          }
-                                          this.onMouseWheel(e, y)
-                                    },
-                                    false
-                              );
-                        }
-
-                  },
-                  onMouseWheel(e, deltaY) {
-                        let event = e || window.event
-                        let main = this.$refs.main
-                        let offset = this.$refs.main.getBoundingClientRect().top
-
-                        if (!this.ifInSlide(deltaY, offset)) return
-
-                        this.scrollOffset = document.documentElement.scrollTop || document.body.scrollTop
-
-                        if (event.preventDefault) {
-                              event.preventDefault()
-                              event.stopPropagation()
-                        } else if (document.all) {
-                              event.cancelBubble = true
-                              event.returnValue = false
-                        } else {
-                              event.stopPropagation()
-                        }
-
-                        if (window.location.pathname != '/' || this.slidesLock) return
-
-                        this.slidesLock = true
-
-                        this.doScroll(deltaY)
-                  },
-                  doScroll(deltaY) {
-                        let header = document.querySelector('.blog-header')
-                        let home = document.querySelector('.blog-home')
-                        let about = document.querySelector('#about')
-                        if (deltaY > 0) {
-                              window.scrollTo(0, about.scrollHeight)
-                              header.classList.remove('hide')
-                        } else {
-                              window.scrollTo(0, 0)
-                              header.classList.add('hide')
-                        }
-
-                        this.interval = setTimeout(() => {
-                              this.slidesLock = false
-                        }, 500)
-                  },
-                  ifInSlide(deltaY, offset) {
-                        let about = document.querySelector('#about')
-                        return deltaY > 0 ? (-offset < about.offsetHeight - 1) : (-offset <= about.scrollHeight + 80)
-                  }
-            },
-      }
+            ifInSlide(deltaY, offset) {
+                let home = document.querySelector('.blog-home')
+                return deltaY > 0 ?
+                    (-offset < home.offsetHeight - 1) :
+                    (-offset <= home.scrollHeight + 80)
+            }
+        },
+    }
 </script>
 
 <style lang="scss" scoped>
-      @import '../styles/values.scss';
+    @import '../styles/values.scss';
 
-      .blog-home {
-            @extend .flex;
+    .blog-home {
+        @extend .flex;
+        width: 100%;
+        height: 100vh;
+
+        &::before {
+            content: '';
+            z-index: -10;
             width: 100%;
-            height: 100vh;
+            height: 100%;
+            // background-image: url(https://i.loli.net/2020/09/11/ElcNrtJVkPWamOo.jpg);
+            background-image: url("../../public/assets/img/background.jpg");
+            background-size: cover;
+            background-repeat: no-repeat;
+            background-position: center top;
+            filter: brightness(80%);
+            position: absolute;
+            top: 0;
+            left: 0;
+        }
 
-            &::before {
-                  content: '';
-                  z-index: -10;
-                  width: 100%;
-                  height: 100%;
-                  // background-image: url(https://i.loli.net/2020/09/11/ElcNrtJVkPWamOo.jpg);
-                  background-image: url("../../public/assets/img/background.jpg");
-                  background-size: cover;
-                  background-repeat: no-repeat;
-                  background-position: center top;
-                  filter: brightness(80%);
-                  position: absolute;
-                  top: 0;
-                  left: 0;
-            }
+        .home-content {
+            @extend .flex-column;
 
-            .home-content {
-                  @extend .flex-column;
-
-                  // .blog-icon {
-                  //       width: 260px;
-                  //       height: 260px;
-                  //       // background-image: url(https://i.loli.net/2020/09/12/wdN45JRBkQtsfhj.jpg);
-                  //       background-image: url("../../public/assets/img/logo.jpg");
-                  //       background-size: cover;
-                  //       box-sizing: border-box;
-                  //       border-radius: 20px;
-                  //       border: 5px solid white;
-                  //       display: none;
-                  // }
-
-                  .blog-description {
-                        @extend .flex-column;
-                        margin-top: 150px;
-
-                        h1 {
-                              font-size: 46px;
-                              color: rgb(148, 188, 218);
-                        }
-
-                        p {
-                              color: rgb(209, 209, 209);
-                              text-align: center;
-                              line-height: 26px;
-                        }
-                  }
-            }
-      }
-
-      @media screen and (max-width: 768px) {
+            // .blog-icon {
+            //       width: 260px;
+            //       height: 260px;
+            //       // background-image: url(https://i.loli.net/2020/09/12/wdN45JRBkQtsfhj.jpg);
+            //       background-image: url("../../public/assets/img/logo.jpg");
+            //       background-size: cover;
+            //       box-sizing: border-box;
+            //       border-radius: 20px;
+            //       border: 5px solid white;
+            //       display: none;
+            // }
 
             .blog-description {
-                  margin-top: 80px !important;
+                @extend .flex-column;
+                margin-top: 150px;
 
-                  p {
-                        width: 80%;
-                  }
-            }
-      }
+                h1 {
+                    font-size: 46px;
+                    color: rgb(148, 188, 218);
+                }
 
-      @media screen and (max-width: 320px) {
-            .blog-description h1 {
-                  font-size: 40px;
+                p {
+                    color: rgb(209, 209, 209);
+                    text-align: center;
+                    line-height: 26px;
+                }
             }
-      }
+        }
+    }
+
+    .blog-articles {
+        margin-top: 5rem;
+    }
+
+    @media screen and (max-width: 768px) {
+
+        .blog-description {
+            margin-top: 80px !important;
+
+            p {
+                width: 80%;
+            }
+        }
+    }
+
+    @media screen and (max-width: 320px) {
+        .blog-description h1 {
+            font-size: 40px;
+        }
+    }
 </style>
--- docs/.vuepress/theme/layouts/Layout.vue
+++ docs/.vuepress/theme/layouts/Layout.vue
@@ -1,9 +1,49 @@
 <template>
-      <div class="theme-container">
-            <blog-header></blog-header>
-            <blog-index></blog-index>
-            <blog-home></blog-home>
-      </div>
+    <div class="theme-container">
+        <blog-index></blog-index>
+        <div class="blog-home">
+            <div class="home-content">
+                <!-- <div class="blog-icon"></div> -->
+                <div class="blog-description">
+                    <h1>{{ $page.frontmatter.title }}</h1>
+                    <p>本博客网站基于HTML5开发，若无法正常访问请更换高版本浏览器</p>
+                </div>
+            </div>
+            <drop-down></drop-down>
+            <div class="cloud">
+                <svg class="waves" viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
+                    <defs>
+                        <path id="gentle-wave"
+                            d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" fill="white">
+                        </path>
+                    </defs>
+                    <g class="parallax">
+                        <use xlink:href="#gentle-wave" x="48" y="0" opacity="0.7">
+                            <path id="gentle-wave"
+                                d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z"
+                                fill="white"></path>
+                        </use>
+                        <use xlink:href="#gentle-wave" x="48" y="3" opacity="0.5">
+                            <path id="gentle-wave"
+                                d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z"
+                                fill="white"></path>
+                        </use>
+                        <use xlink:href="#gentle-wave" x="48" y="5" opacity="0.3">
+                            <path id="gentle-wave"
+                                d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z"
+                                fill="white"></path>
+                        </use>
+                        <use xlink:href="#gentle-wave" x="48" y="7">
+                            <path id="gentle-wave"
+                                d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z"
+                                fill="white"></path>
+                        </use>
+                    </g>
+                </svg>
+            </div>
+        </div>
+        <blog-home></blog-home>
+    </div>
 </template>
 
 <script>
